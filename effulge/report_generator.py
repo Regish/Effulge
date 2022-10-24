37da93e8346ec3fc20f1f78fd6773cfce6ca3603
Regish <regishdhanush@gmail.com>
2022-10-24 19:52:51 +0530

    apply pylint suggestions

--- effulge/report_generator.py
+++ effulge/report_generator.py
@@ -7,13 +7,13 @@ Module to Generate Variance Report
 
 """
 
-from pyspark.sql.functions import sum
-from pyspark.sql.functions import desc
-from pandas import ExcelWriter
-from effulge import summarize_variance
+from pyspark.sql.functions import sum     # pylint: disable=import-error, redefined-builtin
+from pyspark.sql.functions import desc    # pylint: disable=import-error
+from pandas import ExcelWriter            # pylint: disable=import-error
+from effulge import summarize_variance    # pylint: disable=no-name-in-module
 
 
-def save_variance_report(variance_df, source_df, target_df, super_key, file_name, **kwargs):
+def save_variance_report(variance_df, source_df, target_df, super_key, file_name, **kwargs):    # pylint: disable=too-many-locals
     """
     Function to create Excel report from Effulge Variance Dataframe
 
@@ -50,7 +50,7 @@ def save_variance_report(variance_df, source_df, target_df, super_key, file_name
     excel_options['strings_to_urls'] = False
     excel_engine_kwargs = {'options':excel_options}
     excel_writer = ExcelWriter(
-        file_name + '.xlsx', 
+        file_name + '.xlsx',
         engine='xlsxwriter',
         engine_kwargs=excel_engine_kwargs
     )
@@ -70,7 +70,8 @@ def save_variance_report(variance_df, source_df, target_df, super_key, file_name
     #
     # create Excel sheet for varying attributes
     for column in _list_of_attributes :
-        _focused_spark_df = _focused_variance_for_each_attribute(variance_df, source_df, target_df, super_key, column, **kwargs)
+        _focused_spark_df = _focused_variance_for_each_attribute(variance_df, source_df, target_df,
+                                                                 super_key, column, **kwargs)
         _focused_panda_df = _focused_spark_df.toPandas()
         _focused_panda_df.to_excel(excel_writer, sheet_name=column, index=False)
     # save Excel file
@@ -80,7 +81,7 @@ def save_variance_report(variance_df, source_df, target_df, super_key, file_name
 def _sort_by_most_impacted_attributes(variance_df):
     """
     Function to identify impact of each column
-    and sort them based on thier severity
+    and sort them based on their severity
 
     Parameters:
         Name: variance_df
@@ -99,7 +100,7 @@ def _sort_by_most_impacted_attributes(variance_df):
 
     """
     out_df = variance_df.selectExpr(
-            "explode(EFFULGE_VARIANCE_PROVOKER) as _attribute_name" , 
+            "explode(EFFULGE_VARIANCE_PROVOKER) as _attribute_name",
             "EFFULGE_VARIANCE_COUNT as _count"
         )\
         .groupBy("_attribute_name")\
@@ -109,12 +110,14 @@ def _sort_by_most_impacted_attributes(variance_df):
     return out_df
 
 
-def _focused_variance_for_each_attribute(variance_df, source_df, target_df, 
-                                        primary_key_list, attribute_name, 
-                                        src_prefix='src', tgt_prefix='tgt'):
+def _focused_variance_for_each_attribute(
+            #pylint: disable=too-many-arguments
+            variance_df, source_df, target_df, primary_key_list,
+            attribute_name, src_prefix='src', tgt_prefix='tgt'):
+    #pylint: disable=line-too-long
     """
-    Function to fetch all variances for a a given column.
-    
+    Function to fetch all variances for a given column.
+
     Parameters:
         Name: variance_df
         Type:    pyspark.sql.dataframe.DataFrame
@@ -143,19 +146,23 @@ def _focused_variance_for_each_attribute(variance_df, source_df, target_df,
     +------------------------+---------+------------------------+--------------------------------------+--------------------------------------+
 
     """
-    _filtered_records = variance_df.where(f"ARRAY_CONTAINS( EFFULGE_VARIANCE_PROVOKER, '{attribute_name}')")
+    _filtered_records = variance_df\
+        .where(f"ARRAY_CONTAINS( EFFULGE_VARIANCE_PROVOKER, '{attribute_name}')")
     focused_df = _filtered_records\
         .join(
-            source_df.selectExpr(*primary_key_list, f"{attribute_name} as _{src_prefix}_{attribute_name}")  ,
+            source_df.selectExpr(*primary_key_list,
+                                 f"{attribute_name} as _{src_prefix}_{attribute_name}")  ,
             [ *primary_key_list ] ,
             "left"
         ).join(
-            target_df.selectExpr(*primary_key_list, f"{attribute_name} as _{tgt_prefix}_{attribute_name}")  ,
+            target_df.selectExpr(*primary_key_list,
+                                 f"{attribute_name} as _{tgt_prefix}_{attribute_name}")  ,
             [ *primary_key_list ] ,
             "left"
         ).selectExpr(
-            *primary_key_list , f"_{src_prefix}_{attribute_name}" , f"_{tgt_prefix}_{attribute_name}"
+            *primary_key_list,
+            f"_{src_prefix}_{attribute_name}",
+            f"_{tgt_prefix}_{attribute_name}"
         )
     #
     return focused_df
-
